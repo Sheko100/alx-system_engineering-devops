3cda7ae68db0e4ec9a51b19dc9e40bb44853a028
Shaker Sharabi <sheko100@icloud.com>
2023-11-24 22:22:24 +0200

    Add comment lines

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,5 @@
+# creates a school in the /tmp directory with the content "I love Puppet"
+# with "www-data" as the group and owner, and with 0744 permissions
 file {'/tmp/school':
   ensure  => 'present',
   path    => '/tmp/school',
