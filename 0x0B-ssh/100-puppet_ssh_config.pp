2c35b8bc337c3c540b2028e35581a009281200f9
Shaker Sharabi <sheko100@icloud.com>
2024-02-26 01:37:27 +0200

    Add 100-puppet_ssh_config.pp file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Configures SSH to refuse password auth and to use specific private key
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  match  => '#?   PasswordAuthentication'
+}
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
