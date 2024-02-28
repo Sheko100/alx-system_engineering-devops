96961876684ed4c916edc0c5cb41af6234ccb0ef
Shaker Sharabi <sheko100@icloud.com>
2024-02-28 03:26:45 +0200

    Add update repos

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,10 +1,14 @@
 # ensures that nginx is installed and with particular configuration
+exec { 'apt update':
+  command => '/usr/bin/apt-get update',
+  before  => Package['nginx']
+}
 package { 'nginx':
   ensure   => 'installed',
   provider => 'apt'
 }
 file { 'root content':
-  esnure  => 'present',
+  ensure  => 'present',
   path    => '/var/www/html/index.nginx-debian.html',
   content => 'Hello World!',
   require => Package['nginx']
@@ -15,11 +19,11 @@ file { 'config':
   require => Package['nginx']
 }
 exec { 'listen to 80':
-  command => "/usr/bin/sed -r -i 's|^(\tlisten [^0-9]*)[0-9]+|\180|' /etc/nginx/sites-enabled/default",
+  command => '/usr/bin/sed -r -i \'s|^(\tlisten [^0-9]*)[0-9]+|\180|\' /etc/nginx/sites-enabled/default',
   require => File['config']
 }
-exec { 'redirection'
-  command => "/usr/bin/sed -r -i 's|^(\tlocation /).*$|&\n\n\t\1redirect_me {\n\t\t\treturn 301 https://linkedin.com/in/shaker-sharabi/;\n\t\t}|' /etc/nginx/sites-enabled/default",
+exec { 'redirection':
+  command => '/usr/bin/sed -r -i \'s|^(\tlocation /).*$|&\n\n\t\1redirect_me {\n\t\t\treturn 301 https://linkedin.com/in/shaker-sharabi/;\n\t\t}|\' /etc/nginx/sites-enabled/default',
   unless  => "/usr/bin/grep -q 'location /redirect_me' /etc/nginx/sites-enabled/default",
   require => File['config']
 }
